e234219b51419ec841959eced9b58a705b7af549
Antoine Marchive <antoine.marchive@orange.fr>
2025-02-24 10:33:15 +0100

    explications userProfile.js

---
+++ force-app/main/default/lwc/userProfile/userProfile.js
@@ -0,0 +1,22 @@
+import { LightningElement, wire, track } from 'lwc';
+import getUserProfile from '@salesforce/apex/UserProfileController.getUserProfile';
+
+export default class UserProfile extends LightningElement {
+    @track userProfile;
+    isAdmin = false;
+    isCommercial = false;
+
+    @wire(getUserProfile)
+    //@wire(getUserProfile) : Appelle la méthode Apex et stocke le profil utilisateur.
+    wiredProfile({ error, data }) {
+        if (data) {
+            this.userProfile = data;
+            this.isAdmin = data === 'System Administrator';
+            //this.isAdmin = data === 'System Administrator' : Vérifie si l'utilisateur est admin
+            this.isCommercial = data === 'Commercial';
+            //this.isCommercial = data === 'Commercial' : Vérifie si l'utilisateur est commercial
+        } else if (error) {
+            console.error('Erreur lors de la récupération du profil utilisateur', error);
+        }
+    }
+}
